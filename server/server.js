f2a136989d99817a7f99d1d54e50a155ddd1068e
sagnikroy <sagnik.sinha@edupristine.ca>
2016-04-17 13:34:04 +0530

    Fix multiple connection issues

--- server/server.js
+++ server/server.js
@@ -8,18 +8,25 @@ var app;
 // Create a simple server
 var server = net.createServer(function (conn) {
 
-    app = express();
-
-    app.use(bodyParser.urlencoded({ extended: false }));
-    app.use(bodyParser.json());
+   
 
     conn.on("error", function(err) {
       console.log("Connection Closed: " + err.stack);
       exp_server.close();
+	  server.close();
+	  server.listen(config.PORT_NET,
+		  config.ALLOWED_IPS,
+		  config.HOST,
+		  function () {
+			console.log("Server: Listening");
+		});
+		app = express();
+		conn.destroy();
     });
 
     conn.on('end', function() {
       console.log("Connection Closed Properly");
+	  conn.destroy();
       exp_server.close();
     });
 
@@ -35,6 +42,17 @@ var server = net.createServer(function (conn) {
             { response: "Hey there client!" }
         )
     );
+	
+	conn.pipe(conn);
+	
+	app = express();
+
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+	
+	var exp_server = app.listen(config.PORT_APP, function() {
+      console.log("Express started");
+    });
 
     app.post('/command', function(req, res) {
       conn.write(JSON.stringify({response: req.body.command}));
@@ -42,9 +60,6 @@ var server = net.createServer(function (conn) {
       res.end("yes");
     });
 
-    var exp_server = app.listen(config.PORT_APP, function() {
-      console.log("Express started");
-    });
 });
 
 // Listen for connections
